0f24666125ec5b8bc467e8adeafb16a083d99246
Laurence042 <qq2120286535@gmail.com>
2021-05-08 00:44:48 +0800

    add customize watermarkBit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      color="blue darken-3"
-      dark
-    >
+    <v-app-bar app color="blue darken-3" dark>
       <v-file-input
         accept="image/*"
         label="选择待加工图片"
@@ -19,6 +14,17 @@
       ></v-file-input>
 
       <v-btn @click="process" text>执行</v-btn>
+
+      <v-slider
+        v-model="watermarkBits"
+        label="水印位数"
+        min="2"
+        max="4"
+      ></v-slider>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-text-field type="number" label="水印位数" v-model="watermarkBits"></v-text-field> -->
 
       <v-btn
         href="https://github.com/Laurence-042/lsb-watermark"
@@ -53,6 +59,20 @@
                   <li>点击顶栏上的“执行”按钮</li>
                 </ol>
               </li>
+              <li>
+                水印位数默认为4，但对于某些图来说这个值高到足以让水印显现，您可能会想将其调到3或2
+                <br />
+                （请不要试图将其调到4以上或者2以下。我试过，那不好玩，所以我用滑条把它固定在了2、3、4这三个值）
+              </li>
+              <li>
+                如果您使用4提取水印时发现还掺杂了原图的元素，那么可能是因为加水印时使用了2或3。
+                <br />
+                简单地调整水印位数然后再次点击执行即可！
+                <br />
+                但加水印时请不要这么做，除非您准备增大水印位数
+                <br />
+                ——因为我很懒，我没用加第三块canvas，加水印时是直接在原canvas上改的
+              </li>
             </ol>
           </div>
 
@@ -81,6 +101,8 @@ import ImageToolKit from "./util/ImageToolKit";
 export default {
   name: "App",
   data: () => ({
+    watermarkBits: 4,
+
     /**图片是否已加载 */
     firstImg: null,
     secondImg: null,
@@ -209,26 +231,23 @@ export default {
         this.second_canvas.height
       );
 
+      let mainPixDivisor = 2 ** this.watermarkBits;
+      let secondPixDivisor = 256 / 2 ** this.watermarkBits;
+
       for (let i = this.dy; i < mainImage.height - this.dy; i++) {
         for (let j = this.dx; j < mainImage.width - this.dx; j++) {
           let mainPix = ImageToolKit.getPixel(mainImage, i, j);
           let secondPix = ImageToolKit.getPixel(secondImage, i, j);
 
-          if (i == 100 && j == 100) {
-            console.log(mainPix);
-            console.log(secondPix);
-          }
-
           mainPix.r =
-            Math.floor(mainPix.r / 4) * 4 + Math.floor(secondPix.r / 64);
+            Math.floor(mainPix.r / mainPixDivisor) * mainPixDivisor +
+            Math.floor(secondPix.r / secondPixDivisor);
           mainPix.g =
-            Math.floor(mainPix.g / 4) * 4 + Math.floor(secondPix.g / 64);
+            Math.floor(mainPix.g / mainPixDivisor) * mainPixDivisor +
+            Math.floor(secondPix.g / secondPixDivisor);
           mainPix.b =
-            Math.floor(mainPix.b / 4) * 4 + Math.floor(secondPix.b / 64);
-
-          if (i == 100 && j == 100) {
-            console.log(mainPix);
-          }
+            Math.floor(mainPix.b / mainPixDivisor) * mainPixDivisor +
+            Math.floor(secondPix.b / secondPixDivisor);
 
           ImageToolKit.setPixel(mainImage, i, j, mainPix);
         }
@@ -242,6 +261,9 @@ export default {
     split() {
       console.log("split");
       let mainCtx = this.main_canvas.getContext("2d");
+      let secondCtx = this.second_canvas.getContext("2d");
+      this.second_canvas.width = this.main_canvas.width;
+      this.second_canvas.height = this.main_canvas.height;
 
       console.log("main:" + mainCtx.width + " " + mainCtx.height);
 
@@ -251,30 +273,31 @@ export default {
         this.main_canvas.width,
         this.main_canvas.height
       );
+      let secondImage = secondCtx.getImageData(
+        0,
+        0,
+        this.second_canvas.width,
+        this.second_canvas.height
+      );
+
+      let mainPixDivisor = 2 ** this.watermarkBits;
+      let secondPixDivisor = 256 / 2 ** this.watermarkBits;
 
       for (let i = 0; i < mainImage.height; i++) {
         for (let j = 0; j < mainImage.width; j++) {
           let mainPix = ImageToolKit.getPixel(mainImage, i, j);
 
-          if (i == 100 && j == 100) {
-            console.log(mainPix);
-          }
+          mainPix.r = (mainPix.r % mainPixDivisor) * secondPixDivisor;
+          mainPix.g = (mainPix.g % mainPixDivisor) * secondPixDivisor;
+          mainPix.b = (mainPix.b % mainPixDivisor) * secondPixDivisor;
 
-          mainPix.r = (mainPix.r % 4) * 64;
-          mainPix.g = (mainPix.g % 4) * 64;
-          mainPix.b = (mainPix.b % 4) * 64;
-
-          if (i == 100 && j == 100) {
-            console.log(mainPix);
-          }
-
-          ImageToolKit.setPixel(mainImage, i, j, mainPix);
+          ImageToolKit.setPixel(secondImage, i, j, mainPix);
         }
         // console.log(i + "/" + mainImage.height);
       }
-      mainCtx.putImageData(mainImage, 0, 0);
+      secondCtx.putImageData(secondImage, 0, 0);
 
-      this.imageOut = this.main_canvas.toDataURL();
+      this.imageOut = this.second_canvas.toDataURL();
       this.isImageProcessed = true;
     },
   },
